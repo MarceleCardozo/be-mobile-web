b30dab37de9d33a3b148b6d63ab67dadb6f46e21
Marcele Cardozo <marcelecardozo@gmail.com>
2024-07-07 19:25:22 -0300

    refactor: adjusting styled

--- src/components/search/styles.tsx
+++ src/components/search/styles.tsx
@@ -3,16 +3,24 @@ import styled from "styled-components";
 const SearchWrapperStyled = styled.div`
   display: flex;
   align-items: center;
-  padding: 12px 16px;
   border: 1px solid ${(props) => props.theme.colors.neutral.gray10};
   border-radius: 8px;
   background-color: ${(props) => props.theme.colors.neutral.white};
   justify-content: space-between;
+
+  img {
+    padding-right: 16px; 
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const SearchInputStyled = styled.input`
   background-color: transparent;
   border: none;
+  padding: 12px 16px;
   font-family: ${(props) => props.theme.typography.h3.fontFamily};
   font-weight: ${(props) => props.theme.typography.h3.fontWeight};
   font-size: ${(props) => props.theme.typography.h3.fontSize};
